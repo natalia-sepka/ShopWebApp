ad0f0cef168f6285119c7f8df1a088affe7aff7e
natalia.sepka <natalia.sepka@gmail.com>
2023-10-11 20:12:57 +0200

    Create repository.

--- shopWebApp-BE/product-service/src/main/java/com/example/productservice/entity/Product.java
+++ shopWebApp-BE/product-service/src/main/java/com/example/productservice/entity/Product.java
@@ -1,26 +1,31 @@
 package com.example.productservice.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.Map;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
 public class Product {
+    @Id
+    @GeneratedValue(generator = "products_id_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "products_id_seq", sequenceName = "products_id_seq", allocationSize = 1)
     private long id;
     private String uid;
     private boolean activate;
+    @Column(name = "product_name")
     private String name;
     private String mainDesc;
     private String descHtml;
     private float price;
     private String[] imageUrls;
-    private Map<String, String> parameters;
+    private String parameters;
     private LocalDate createAd;
 }
--- shopWebApp-BE/product-service/src/main/java/com/example/productservice/entity/ProductEntity.java
+++ shopWebApp-BE/product-service/src/main/java/com/example/productservice/entity/ProductEntity.java
@@ -1,5 +1,40 @@
 package com.example.productservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Table(name = "products")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProductEntity extends Product {
+    @ManyToOne
+    @JoinColumn(name = "category_parameters")
     private Category category;
+
+    public ProductEntity(
+            long id,
+            String uid,
+            boolean activate,
+            String name,
+            String mainDesc,
+            String descHtml,
+            float price,
+            String[] imageUrls,
+            String parameters,
+            LocalDate createAt,
+            Category category
+    ) {
+        super(id, uid, activate, name, mainDesc, descHtml, price, imageUrls, parameters, createAt);
+        this.category = category;
+    }
+
 }
