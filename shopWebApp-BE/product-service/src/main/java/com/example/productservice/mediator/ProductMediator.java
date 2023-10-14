6d2d7bb0aaeab6200b41075b30e35d09950fff27
natalia.sepka <natalia.sepka@gmail.com>
2023-10-14 20:15:08 +0200

    ProductEntityToSimpleProduct translator create.

--- shopWebApp-BE/product-service/src/main/java/com/example/productservice/mediator/ProductMediator.java
+++ shopWebApp-BE/product-service/src/main/java/com/example/productservice/mediator/ProductMediator.java
@@ -1,19 +1,23 @@
 package com.example.productservice.mediator;
 
 import com.example.productservice.entity.ProductEntity;
+import com.example.productservice.entity.SimpleProductDTO;
 import com.example.productservice.service.ProductService;
+import com.example.productservice.translator.ProductEntityToSimpleProduct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ProductMediator {
     private final ProductService productService;
+    private final ProductEntityToSimpleProduct productEntityToSimpleProduct;
     public ResponseEntity<?> getProduct(
             int page,
             int limit,
@@ -25,7 +29,6 @@ public class ProductMediator {
             String sort,
             String order
     ) {
-        long totalCount = productService.countActiveProducts(name, category,maxPrice, minPrice);
         List<ProductEntity> product = productService.getProduct(
                 name,
                 category,
@@ -44,6 +47,14 @@ public class ProductMediator {
                 throw new RuntimeException(e);
             }
         }
-        return ResponseEntity.ok().header("X-Total-Count", String.valueOf(totalCount)).body(product);
+        if (name == null || name.isEmpty() || data == null || data.isEmpty()) {
+            List<SimpleProductDTO> simpleProductDTOS = new ArrayList<>();
+            long totalCount = productService.countActiveProducts(name, category,maxPrice, minPrice);
+            product.forEach(value -> {
+                simpleProductDTOS.add(productEntityToSimpleProduct.toSimpleProduct(value));
+            });
+            return ResponseEntity.ok().header("X-Total-Count", String.valueOf(totalCount)).body(simpleProductDTOS);
+        }
+        return ResponseEntity.ok().body(product);
     }
 }
