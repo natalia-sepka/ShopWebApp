53f679a8b61cefcbd92f0958b3d62e541d2f272e
natalia.sepka <natalia.sepka@gmail.com>
2023-10-17 11:41:28 +0200

    Custom ImageResponse add.

--- shopWebApp-BE/fileservice/src/main/java/com/example/fileservice/facade/entity/ImageDTO.java
+++ shopWebApp-BE/fileservice/src/main/java/com/example/fileservice/facade/entity/ImageDTO.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-@Builder
+
 @Getter
 @Setter
-public class ImageEntity {
-    private long id;
+@Builder
+public class ImageDTO {
     private String uuid;
-    private String path;
-    private boolean isUsed;
     private LocalDate createAt;
+
 }
--- shopWebApp-BE/fileservice/src/main/java/com/example/fileservice/mediator/MediatorImage.java
+++ shopWebApp-BE/fileservice/src/main/java/com/example/fileservice/mediator/MediatorImage.java
@@ -1,6 +1,8 @@
 package com.example.fileservice.mediator;
 
+import com.example.fileservice.facade.entity.ImageDTO;
 import com.example.fileservice.facade.entity.ImageEntity;
+import com.example.fileservice.facade.entity.ImageResponse;
 import com.example.fileservice.service.FtpService;
 import com.example.fileservice.service.ImageService;
 import lombok.AllArgsConstructor;
@@ -17,10 +19,10 @@ public class MediatorImage {
     private ResponseEntity<?> saveImage(MultipartFile file) {
         try {
             ImageEntity imageEntity = ftpService.uploadFileToFtp(file);
-            imageService.save(imageEntity);
-            return ResponseEntity.ok("");
+            imageEntity = imageService.save(imageEntity);
+            return ResponseEntity.ok(ImageDTO.builder().uuid(imageEntity.getUuid()).createAt(imageEntity.getCreateAt()));
         } catch (RuntimeException e) {
-            return ResponseEntity.status(400).body("Can not save file");
+            return ResponseEntity.status(400).body(new ImageResponse("Can not save file."));
         }
 
     }
