23ff9d3e8bbdefdd33a73bc0d80cfbbb1eb479b2
natalia.sepka <natalia.sepka@gmail.com>
2023-10-18 16:04:49 +0200

    File-service db connection conf.

--- shopWebApp-BE/fileservice/src/main/java/com/example/fileservice/entity/ImageEntity.java
+++ shopWebApp-BE/fileservice/src/main/java/com/example/fileservice/entity/ImageEntity.java
@@ -1,17 +1,25 @@
 package com.example.fileservice.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "image_data")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ImageEntity {
+    @Id
+    @GeneratedValue(generator ="image_data_id_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "image_data_id_seq", sequenceName = "image_data_id_seq", allocationSize = 1)
     private long id;
     private String uuid;
     private String path;
+    @Column(name = "isused")
     private boolean isUsed;
+    @Column(name = "createat")
     private LocalDate createAt;
 }
