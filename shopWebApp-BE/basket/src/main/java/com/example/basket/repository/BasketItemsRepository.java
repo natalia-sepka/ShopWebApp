3db7cf011ec0384056d49ed72826047a041eba45
natalia.sepka <natalia.sepka@gmail.com>
2023-11-28 15:44:11 +0100

    Adding product to the basket add.

--- shopWebApp-BE/basket/src/main/java/com/example/basket/repository/BasketItemsRepository.java
+++ shopWebApp-BE/basket/src/main/java/com/example/basket/repository/BasketItemsRepository.java
@@ -2,7 +2,6 @@ package com.example.basket.repository;
 
 import com.example.basket.entity.Basket;
 import com.example.basket.entity.BasketItems;
-import com.example.basket.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
--- shopWebApp-BE/basket/src/main/java/com/example/basket/repository/BasketRepository.java
+++ shopWebApp-BE/basket/src/main/java/com/example/basket/repository/BasketRepository.java
@@ -1,7 +1,6 @@
 package com.example.basket.repository;
 
 import com.example.basket.entity.Basket;
-import com.example.basket.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
