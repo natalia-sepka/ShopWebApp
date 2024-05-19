87f037d3d5588e532ad6727e66e61559278939cf
natalia.sepka <natalia.sepka@gmail.com>
2024-05-19 20:29:06 +0200

    Basket and basket-product components styling

--- shopWebApp-FE/src/app/modules/basket/basket.module.ts
+++ shopWebApp-FE/src/app/modules/basket/basket.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { BasketRoutingModule } from './basket-routing.module';
 import { BasketComponent } from './components/basket/basket.component';
 import { BasketProductComponent } from './components/basket/basket-product/basket-product.component';
-
+import { SharedModule } from '../shared/shared.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [
-    BasketComponent,
-    BasketProductComponent
-  ],
-  imports: [
-    CommonModule,
-    BasketRoutingModule
-  ]
+  declarations: [BasketComponent, BasketProductComponent],
+  imports: [SharedModule, BasketRoutingModule, MatTooltipModule],
 })
-export class BasketModule { }
+export class BasketModule {}
--- shopWebApp-FE/src/app/modules/basket/components/basket/basket.component.ts
+++ shopWebApp-FE/src/app/modules/basket/components/basket/basket.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { BasketProduct } from '../../../core/models/basket.module';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.scss']
+  styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent {
-
+  basketProducts: BasketProduct[] = [
+    {
+      name: 'Testowy produkt',
+      price: 150,
+      imageUrl: 'https:/i/imgur.com/kHeKKij.jpg',
+      quantity: 2,
+      uuid: 'test',
+      summaryPrice: 300,
+    },
+    {
+      name: 'Testowy produkt 2',
+      price: 150,
+      imageUrl: 'https:/i/imgur.com/kHeKKij.jpg',
+      quantity: 2,
+      uuid: 'test',
+      summaryPrice: 300,
+    },
+  ];
+  summaryPrice = 600;
 }
