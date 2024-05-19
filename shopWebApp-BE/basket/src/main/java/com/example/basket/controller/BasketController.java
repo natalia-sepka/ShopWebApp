196acd65e6ddc8f38ee579a72ba3c3395fdde0cd
natalia.sepka <natalia.sepka@gmail.com>
2024-05-19 17:42:30 +0200

    Exception handling add.

--- shopWebApp-BE/basket/src/main/java/com/example/basket/controller/BasketController.java
+++ shopWebApp-BE/basket/src/main/java/com/example/basket/controller/BasketController.java
@@ -3,10 +3,13 @@ package com.example.basket.controller;
 
 import com.example.basket.entity.BasketItemAddDTO;
 import com.example.basket.entity.Response;
+import com.example.basket.exceptions.BasketItemDoesntExistException;
+import com.example.basket.exceptions.NoBasketInfoException;
 import com.example.basket.service.BasketService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,4 +36,18 @@ public class BasketController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> getItems(HttpServletRequest request) { return basketService.getItems(request); }
+
+    @ExceptionHandler(BasketItemDoesntExistException.class)
+    private ResponseEntity<Response> handleException(BasketItemDoesntExistException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new Response(exception.getMessage()));
+    }
+
+    @ExceptionHandler(NoBasketInfoException.class)
+    private ResponseEntity<Response> handleException(NoBasketInfoException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new Response(exception.getMessage()));
+    }
 }
